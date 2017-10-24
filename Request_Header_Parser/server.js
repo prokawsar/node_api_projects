cd280055c43eb977c2c27a4514b96858fc6f9977
Kawsar Ahmed <mkawsar10@gmail.com>
2017-10-24 23:21:02 +0600

    commit

---
+++ Request_Header_Parser/server.js
@@ -0,0 +1,36 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+const requestIp = require('request-ip');
+
+var app = express();
+
+// Read Header
+
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+app.use(express.static('public'));
+app.use(requestIp.mw());
+
+app.get("/", function (request, response) {
+  
+  var ip = request.clientIp;
+  var lang = request.headers["accept-language"].split(",");
+  lang = lang[0];
+  
+  var soft = request.headers['user-agent'];
+  soft = soft.substring(soft.indexOf("(")+1, soft.indexOf(")"));
+  
+  response.write(JSON.stringify( {ipaddress: ip, language: lang, software: soft }));
+  // response.write(soft);
+  
+  response.end();
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
